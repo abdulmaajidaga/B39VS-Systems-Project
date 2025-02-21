21d07d6055b6f9f0e38fee84dbeddb8ec52fc533
abdulmaajidaga <amaajidking@gmail.com>
2025-02-21 09:14:39 +0400

    launch files for rviz and urdf

---
+++ b39vs_ws/src/hazmat_control/launch/arm_launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': open('src/hazmat_control/resource/robot_description/urdf/arm.urdf').read()}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', 'src/hazmat_control/resource/rviz/arm_config.rviz']
+        ),
+    ])
--- b39vs_ws/src/hazmat_control/setup.py
+++ b39vs_ws/src/hazmat_control/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/arm_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +21,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'mecanum = hazmat_control.mecanum:main'
+            'mecanum = hazmat_control.mecanum:main',
+            'visualiser = hazmat_control.arm_visualiser:main'
         ],
     },
 )
