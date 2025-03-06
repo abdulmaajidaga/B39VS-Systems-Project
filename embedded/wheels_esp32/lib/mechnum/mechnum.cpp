87d708dfbfa131f81607c4f686bc716da68d85ce
Lel <lm2102@hw.ac.uk>
2025-03-06 14:08:00 +0400

    working lib

--- embedded/wheels_esp32/lib/mechnum/mechnum.cpp
+++ embedded/wheels_esp32/lib/mechnum/mechnum.cpp
@@ -4,56 +4,105 @@
 #include <I2Cdev.h>
 #include <MPU6050.h>
 
-Pwm pwm = Pwm();
+// Global array of pointers to mechnum instances for ISR access
+mechnum* mechnumPtr[4] = { nullptr, nullptr, nullptr, nullptr };
 
-void mechnum::attach(int EN_Pin ,int IN1_Pin ,int IN2_Pin ,int ENC1_Pin ,int ENC2_Pin){
+Pwm pwm = Pwm();
+int instanceCounter = 0;
+
+// Attach function to set up pins and interrupt
+void mechnum::attach(int EN_Pin, int IN1_Pin, int IN2_Pin, int ENC1_Pin, int ENC2_Pin) {
     EN = EN_Pin;
     IN1 = IN1_Pin;
     IN2 = IN2_Pin;
     EN1 = ENC1_Pin;
     EN2 = ENC2_Pin;
+    pulsecount = 0;  // Initialize pulse count
 
     pwm.attach(EN);
-    pinMode(IN1 ,OUTPUT);
-    pinMode(IN2 ,OUTPUT);
-    pinMode(EN1,INPUT_PULLUP);
-    pinMode(EN2,INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(EN1), enc1_ISR, RISING);
-    
-}
-void IRAM_ATTR enc1_ISR() { 
-    if (digitalRead(EN2) == HIGH) pulseCount1++; // Forward
-    else pulseCount1--; // Backward
-  }
-float mechnum::get_speed_enc(volatile int &pulsecount) {
-    //TODO : USE ENCODERS TO GET SPEED MESUREMENT 
-    int pulsesPerRevolution = 150; // Adjust based on your motor specs
-    float speed_encoder = (pulseCount / (float)pulsesPerRevolution); // Convert to RPM
-    pulseCount = 0; // Reset pulse count for next cycle
-    return speed_encoder;
-}
-float mechnum::get_speed_imu() {
-    //TODO : USE IMU SPEED 
-    return speed_imu;
-}
-void mechnum::set_speed(int speed) {
-    //TODO : USE get_speed and set_power to set_speed
-    // PID ??
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(EN1, INPUT_PULLUP);
+    pinMode(EN2, INPUT_PULLUP);
+
+    // Store instance pointer and attach ISRs based on instance count
+    if (instanceCounter < 4) {
+        mechnumPtr[instanceCounter] = this;
+        switch (instanceCounter) {
+            case 0:
+                attachInterrupt(digitalPinToInterrupt(EN1), enc_ISR0, RISING);
+                break;
+            case 1:
+                attachInterrupt(digitalPinToInterrupt(EN1), enc_ISR1, RISING);
+                break;
+            case 2:
+                attachInterrupt(digitalPinToInterrupt(EN1), enc_ISR2, RISING);
+                break;
+            case 3:
+                attachInterrupt(digitalPinToInterrupt(EN1), enc_ISR3, RISING);
+                break;
+        }
+        instanceCounter++;
+    }
 }
 
-void mechnum::maintain_speed() {
-    //TODO: PID maintiain feedback look
-    
+// Separate ISRs for each motor
+void mechnum::enc_ISR0() {
+    if (digitalRead(mechnumPtr[0]->EN2) == HIGH)
+        mechnumPtr[0]->pulsecount++;
+    else
+        mechnumPtr[0]->pulsecount--;
+}
+
+void mechnum::enc_ISR1() {
+    if (digitalRead(mechnumPtr[1]->EN2) == HIGH)
+        mechnumPtr[1]->pulsecount++;
+    else
+        mechnumPtr[1]->pulsecount--;
+}
+
+void mechnum::enc_ISR2() {
+    if (digitalRead(mechnumPtr[2]->EN2) == HIGH)
+        mechnumPtr[2]->pulsecount++;
+    else
+        mechnumPtr[2]->pulsecount--;
+}
+
+void mechnum::enc_ISR3() {
+    if (digitalRead(mechnumPtr[3]->EN2) == HIGH)
+        mechnumPtr[3]->pulsecount++;
+    else
+        mechnumPtr[3]->pulsecount--;
+}
+
+// Example speed calculation using encoder pulses
+float mechnum::get_speed_enc() {
+    int pulsesPerRevolution = 150;  // Adjust based on your motor specs
+    float speed_encoder = (pulsecount / (float)pulsesPerRevolution);  // Convert to RPM
+    pulsecount = 0;  // Reset pulse count for the next cycle
+    return speed_encoder;
+}
+
+float mechnum::get_speed_imu() {
+    return speed_imu;  // Placeholder
 }
 
 void mechnum::set_power(int powerr) {
     if (powerr >= 0) {
-        digitalWrite(IN1,HIGH);
-        digitalWrite(IN2,LOW);
-        pwm.write(EN,powerr);
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        pwm.write(EN, powerr);
     } else {
-        digitalWrite(IN1,LOW);
-        digitalWrite(IN2,HIGH);
-        pwm.write(EN,-powerr);
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        pwm.write(EN, -powerr);
     }
+}
+
+void mechnum::set_speed(int speed) {
+    // To be implemented
+}
+
+void mechnum::maintain_speed() {
+    // To be implemented
 }
--- embedded/wheels_esp32/lib/mechnum/mechnum.h
+++ embedded/wheels_esp32/lib/mechnum/mechnum.h
@@ -1,28 +1,33 @@
 #ifndef mechnum_h
 #define mechnum_h
+
+#include <Arduino.h>
+
 class mechnum 
 {
     private:
-    //var for lib func
-    float speed_encoder ;
-    float dir;
-    float power;
-    int EN ;
-    int IN1;
-    int IN2;
-    int speed_imu;
-    int EN1;
-    int EN2;
-    int pulsecount;
+        float speed_encoder;
+        float dir;
+        float power;
+        int EN;
+        int IN1;
+        int IN2;
+        int speed_imu;
+        int EN1;
+        int EN2;
+        volatile int pulsecount;  // Make this variable volatile for ISRs
 
     public:
-    void attach(int EN_Pin ,int IN1_Pin ,int IN2_Pin ,int ENC1_Pin ,int ENC2_Pin);
-    float get_speed_enc();
-    float get_speed_imu();
-    void set_speed(int speed);
-    void set_power(int power);
-    void maintain_speed();
-    void IRAM_ATTR enc1_ISR();
+        void attach(int EN_Pin, int IN1_Pin, int IN2_Pin, int ENC1_Pin, int ENC2_Pin);
+        float get_speed_enc();  // Modified to use internal pulsecount
+        float get_speed_imu();
+        void set_speed(int speed);
+        void set_power(int power);
+        void maintain_speed();
+        static void enc_ISR0();  // Separate ISRs for each motor
+        static void enc_ISR1();
+        static void enc_ISR2();
+        static void enc_ISR3();
 };
 
 #endif
