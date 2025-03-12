8532b984f1a469264a06606fae377f352a76cade
abban-fahim <lloydtechno58@gmail.com>
2025-03-13 01:16:31 +0400

    added a launch file for the entire robot (bringup+control+vision+micro_ros)

---
+++ b39vs_ws/src/hazmat_bringup/launch/robot.launch.py
@@ -0,0 +1,53 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
+from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes, Node
+from launch_ros.descriptions import ComposableNode
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    print(os.path.join(get_package_share_directory("depthai_ros_driver"), 'launch'))
+    config_dir = os.path.join(get_package_share_directory('hazmat_vision'), 'config')
+
+    return LaunchDescription([
+        # Node for converting Twist commands to individual wheel velocities
+        Node(
+            package="hazmat_control",
+            executable="mecanum",
+            output='screen',
+        ),
+
+        # Node for micro ROS communication (with ESP)
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            output='screen',
+            arguments=['serial',
+                       '--dev', '/dev/ttyACM0']
+        ),
+
+        # Publishes TF tree from URDF file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory("hazmat_bringup"), 'launch'), "/hazmat_state_publisher.launch.py"], 
+            )
+        ),
+
+        # Start the vision nodes
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory("hazmat_vision"), 'launch'), "/vision.launch.py"], 
+            ),
+            # For later use
+            # launch_arguments={
+            #     "pointcloud.enable": "false",
+            # }.items()
+        ),
+
+    ])
