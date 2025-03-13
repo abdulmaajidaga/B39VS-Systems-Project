3d0671ee62c1c99f25e362ab6acd684a7c485326
abdulmaajidaga <amaajidking@gmail.com>
2025-03-14 01:22:03 +0400

    updated main.cpp

--- embedded/micro_ros_platformio/src/main.cpp
+++ embedded/micro_ros_platformio/src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <micro_ros_platformio.h>
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
 #include <std_msgs/msg/int32.h>
+#include <sensor_msgs/msg/joint_state.h>
 #include <hazmat_msgs/msg/mecanum_cmd.h>
 #include "mecanum.h"
+#include <Adafruit_PWMServoDriver.h>
 
 #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
 #error This example is only available for Arduino framework with serial transport.
@@ -42,18 +45,46 @@ int m1s = 0, m2s = 0, m3s = 0, m4s = 0;
 #define ENC4_A 47
 #define ENC4_B 48
 
+Adafruit_PWMServoDriver PWM = Adafruit_PWMServoDriver(0x40);
+int currentAngle3 = 90;  // Store the last angle for Servo 3
+// Declare the moveToAngle3 function to fix the scope error
+void moveToAngle3(int targetAngle);
+
+#define I2C_SDA 20 
+#define I2C_SCL 19
+
+#define SERVO1_MIN 120   // Servo 1 range (0-270°)
+#define SERVO1_MAX 470  
+#define SERVO2_MIN 120   // Servo 2 range (0-270°)
+#define SERVO2_MAX 470  
+#define SERVO3_NEUTRAL 340  
+#define SERVO3_CW 470       
+#define SERVO3_CCW 210      
+#define SERVO4_MIN 120   // Servo 4 range (0-270°)
+#define SERVO4_MAX 470 
+
+#define ANALOG1_PIN 3
+#define ANALOG2_PIN 12
+#define ANALOG3_PIN 13
+#define ANALOG4_PIN 14
+
 // ROS2 objects
 rcl_timer_t timer;
-rcl_subscription_t mecanum_subscriber;
-rcl_publisher_t mecanum_publisher;
-
-hazmat_msgs__msg__MecanumCmd mecanum_recv_msg;
-hazmat_msgs__msg__MecanumCmd mecanum_msg;
-
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
+
+rcl_publisher_t mecanum_publisher;
+rcl_subscription_t mecanum_subscriber;
+rcl_publisher_t joint_publisher;
+rcl_subscription_t joint_subscriber;
+
+hazmat_msgs__msg__MecanumCmd mecanum_recv_msg;
+hazmat_msgs__msg__MecanumCmd mecanum_msg;
+
+sensor_msgs__msg__JointState joint_cmd_msg;
+sensor_msgs__msg__JointState joint_state_msg;
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();} }
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){} }
@@ -67,6 +98,7 @@ void error_loop() {
 // Timer callback for publishing encoder speeds
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
+
   if (timer != NULL) {
     mecanum_msg.front_left = m1.get_speed_enc();
     mecanum_msg.front_right = m2.get_speed_enc();
@@ -74,6 +106,20 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
     mecanum_msg.rear_right = m4.get_speed_enc();
 
     RCSOFTCHECK(rcl_publish(&mecanum_publisher, &mecanum_msg, NULL));
+
+    // Read analog values and map them to joint positions
+    int analogValue1 = analogRead(ANALOG1_PIN);
+    int analogValue2 = analogRead(ANALOG2_PIN);
+    int analogValue3 = analogRead(ANALOG3_PIN);
+    int analogValue4 = analogRead(ANALOG4_PIN);
+
+    joint_state_msg.position.data[0] = map(analogValue1, 250, 2203, 0, 195);
+    joint_state_msg.position.data[1] = map(analogValue2, 250, 2203, 0, 195);
+    joint_state_msg.position.data[2] = map(analogValue3, 343, 3006, 0, 180);
+    joint_state_msg.position.data[3] = map(analogValue4, 250, 2203, 0, 195);
+
+    // Publish the joint state message
+    RCSOFTCHECK(rcl_publish(&joint_publisher, &joint_state_msg, NULL));
   }
 }
 
@@ -92,8 +138,37 @@ void mecanum_subscription_callback(const void *msgin) {
   m4.set_power(m4s);
 }
 
+void joint_cmd_callback(const void *msgin) {
+    const sensor_msgs__msg__JointState *msg = (const sensor_msgs__msg__JointState *)msgin;
+
+    int angle1 = (int)msg->position.data[0];
+    int angle2 = (int)msg->position.data[1];
+    int angle3 = (int)msg->position.data[2];
+    int angle4 = (int)msg->position.data[3];
+
+
+    if (angle1 >= 15 && angle1 <= 152 && angle2 >= 0 && angle2 <= 270 &&
+        angle3 >= 0 && angle3 <= 360 && angle4 >= 0 && angle4 <= 196) {
+        
+        int pulse1 = map(angle1, 0, 270, SERVO1_MIN, SERVO1_MAX);
+        int pulse2 = map(angle2, 0, 270, SERVO2_MIN, SERVO2_MAX);
+        int pulse4 = map(angle4, 0, 270, SERVO4_MIN, SERVO4_MAX);
+
+        // Servo 3 Handling
+        moveToAngle3(angle3);
+
+        PWM.setPWM(0, 0, pulse1);
+        PWM.setPWM(1, 0, pulse2);
+        PWM.setPWM(3, 0, pulse4);
+    }
+}
+
 void setup() {
   Serial.begin(115200);
+
+  Wire.begin(I2C_SDA, I2C_SCL);
+  PWM.begin();
+  PWM.setPWMFreq(50);
 
   m1.attach(ENA1, IN1_1, IN2_1, ENC1_A, ENC1_B);
   m2.attach(ENB1, IN4_2, IN3_2, ENC2_A, ENC2_B);
@@ -107,18 +182,14 @@ void setup() {
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
   RCCHECK(rclc_node_init_default(&node, "mecanum_node", "", &support));
 
-  RCCHECK(rclc_publisher_init_default(
-    &mecanum_publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(hazmat_msgs, msg, MecanumCmd),
-    "hazmat/encoder_vel"));
+  RCCHECK(rclc_publisher_init_default(&mecanum_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(hazmat_msgs, msg, MecanumCmd), "hazmat/encoder_vel"));
+  RCCHECK(rclc_subscription_init_default(&mecanum_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(hazmat_msgs, msg, MecanumCmd), "/hazmat/wheel_cmd"));
+  RCCHECK(rclc_publisher_init_default(&joint_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState), "arm/joint_state"));
+  RCCHECK(rclc_subscription_init_default(&joint_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState), "arm/joint_cmd"));
+
 
   const unsigned int timer_timeout = 100;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
+  RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback));
 
   RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
@@ -128,16 +199,33 @@ void setup() {
   mecanum_msg.rear_left = 0;
   mecanum_msg.rear_right = 0;
 
-  RCCHECK(rclc_subscription_init_default(
-    &mecanum_subscriber,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(hazmat_msgs, msg, MecanumCmd),
-    "/hazmat/wheel_cmd"));
+  double joint_positions[4] = {0, 0, 0, 0};
+  joint_state_msg.position.data = joint_positions;
+  joint_state_msg.position.size = 4;
+  joint_state_msg.position.capacity = 4;
+  
 
   RCCHECK(rclc_executor_add_subscription(&executor, &mecanum_subscriber, &mecanum_recv_msg, &mecanum_subscription_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &joint_subscriber, &joint_cmd_msg, &joint_cmd_callback, ON_NEW_DATA));
 }
 
 void loop() {
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
   delay(10);
+}
+
+void moveToAngle3(int targetAngle) {
+    int angleDifference = targetAngle - currentAngle3;
+    int moveDuration = map(abs(angleDifference), 0, 360, 0, 1000);
+
+    if (angleDifference > 0) {
+        PWM.setPWM(2, 0, SERVO3_CW);
+    } 
+    else if (angleDifference < 0) {
+        PWM.setPWM(2, 0, SERVO3_CCW);
+    }
+
+    delay(moveDuration);
+    PWM.setPWM(2, 0, SERVO3_NEUTRAL);
+    currentAngle3 = targetAngle;
 }
